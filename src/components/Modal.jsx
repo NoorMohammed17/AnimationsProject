02553223196e3f09325355be8b4dfc6be3fc3a9d
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-09-12 22:07:31 +0530

    reusing using VARIENTS

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,9 +6,16 @@ export default function Modal({ title, children, onClose }) {
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
-        initial={{ opacity: 0, y: 30 }} //opacity'0' means inivisble
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 30 }}
+      variants={{
+        hidden: { opacity: 0, y: 30 },
+        visible : { opacity: 1, y: 0 }
+      }}
+        initial="hidden" //opacity'0' means inivisble
+        animate="visible"
+        exit="hidden"
+        // initial={{ opacity: 0, y: 30 }} //opacity'0' means inivisble
+        // animate={{ opacity: 1, y: 0 }}
+        // exit={{ opacity: 0, y: 30 }}
         open
         className="modal">
         <h2>{title}</h2>
